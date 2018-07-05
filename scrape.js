ded1915a62dbb5f4c5990743d91e5bc3d892ddea
eduardo hernandez <eduardohralejandro@gmail.com>
2018-07-05 22:12:17 +0200

    crape file done, I did not not any especial commit for this homework

--- scrape.js
+++ scrape.js
@@ -1,5 +1,37 @@
 // Your custom scraping script.
-
 // Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
 // for the version we showed in class.
+
+
+document.addEventListener('keypress', imd);
+// I am still understanding this part of eventlistener, I think I don't apply it correctly here, I am still doing my research and readings
+
+function imd() {
+
+  let rows = document.querySelectorAll('tr.odd, tr.even');
+  console.clear();
+  for (let i = 0; i < rows.length; i++) {
+    const info = rows[i];
+    let nameActors = info.querySelector("[itemprop=actor] a span").innerHTML;
+    let characterNames = info.querySelector("td.character").textContent;
+    console.log(`Actor: ${nameActors.trim()}, character: ${characterNames.trim()}`);
+  }
+}
+console.log(imd());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
